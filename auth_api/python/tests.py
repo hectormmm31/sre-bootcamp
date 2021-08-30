ce3b7e22ce11e827e76adf48392476e71d533510
Hector Magana <hectormmm@gmail.com>
2021-08-30 17:44:58 -0500

    UPDATE rename test with better name UPDATE On Docker Image Delete mysql dependencies to shrink image size

--- auth_api/python/tests.py
+++ auth_api/python/tests.py
@@ -14,8 +14,8 @@ class TestStringMethods(unittest.TestCase):
     def test_access_data(self):
         self.assertEqual('You are under protected data', self.validate.access_data('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI'))
 
-    def test_access_incorrect_data(self):
-        self.assertEqual('Error Message: Signature verification failed, status 400 Bad request', self.validate.access_data('15e24a16abfc4eef5faeb806e903f78b188c30e4984a03be4c243312f198d1229ae8759e98993464cf713e3683e891fb3f04fbda9cc40f20a07a58ff4bb00788'))
+    def test_access_data_incorrect(self):
+        self.assertEqual('Error Message: Signature verification failed, status 400 Bad request', self.validate.access_data('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoidmlld2VyLWVycm9yIn0.GhTmZHaQsfGAvB16GoS69OD2eKg5HyD-Ww0c0zKrOCU'))
 
     def test_generate_token_correctHeaders(self):
         self.assertEqual('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYWRtaW4ifQ.BmcZ8aB5j8wLSK8CqdDwkGxZfFwM1X1gfAIN7cXOx9w', self.convert.generate_token('admin', 'secret'))
